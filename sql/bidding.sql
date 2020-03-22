1915f0c6935e196014455ae8ac20000edd070f2d
christine0613 <ngothanhvan.christine@gmail.com>
2020-03-22 23:07:07 +0800

    Update bidding.sql

--- sql/bidding.sql
+++ sql/bidding.sql
@@ -2,11 +2,12 @@ CREATE DATABASE IF NOT EXISTS `bidding` DEFAULT CHARACTER SET latin1 COLLATE lat
 USE `bidding`;
 
 CREATE TABLE if not exists bidItem 
-(productID varchar(100) not null,
-userID1 varchar(10) not null,
-userID2 varchar(10) not null,
+(bidID int not null auto_increment,
+productID varchar(100) not null,
+sellerID varchar(10) not null,
+buyerID varchar(10) not null,
 bidDateTime timestamp not null,
 bidStatus varchar(20) not null,
 bidAmt float not null,
 meetup varchar(100) not null,
-CONSTRAINT bidItem_pk primary key(productID, userID1, userID2) ) ENGINE=INNODB DEFAULT charset=latin1;
+CONSTRAINT bidItem_pk primary key(productID, bidID) ) ENGINE=INNODB DEFAULT charset=latin1;
