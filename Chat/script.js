46bc7923febedc3b91f52a1cc0d976d15d32e18f
sebastianhkx <kxhong.2018@sis.smu.edu.sg>
2020-03-29 14:03:19 +0800

    chat beta

---
+++ Chat/script.js
@@ -0,0 +1,27 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Enter your name')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+    appendMessage(data)
+})
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-chat-message', message)
+    messageInput.value = '' // empty the content everytime send
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
