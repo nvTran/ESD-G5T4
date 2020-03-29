ebf52efdcf194d522d4c602dffd75839fb51a20e
sebastianhkx <kxhong.2018@sis.smu.edu.sg>
2020-03-29 21:58:36 +0800

    update but still broken

--- Chat/server.js
+++ Chat/server.js
@@ -1,4 +1,46 @@
+// const http = require('http') 
+// var app = express()
+// var server = require('http').createServer(app)
+// var io = require('socket.io').listen(server)
+// users = [];
+// connections = [];
+
 const io = require('socket.io')(3000)
+
+// server.listen(process.env.PORT || 3000)
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html')
+// });
+
+// const fs = require('fs')
+// const port = 5500
+
+// const server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'content-Type':'text/html'})
+//     fs.readFile('index.html', function(error, data) {
+//         if (error) {
+//             res.writeHead(404)
+//             res.write('Error: File not found')
+//         }
+//         else {
+//             res.write(data)
+//         }
+//         res.end() 
+
+//     })
+// })
+
+// server.listen(port, function(error) {
+//     if (error) {
+//         console.log('Something is wrong', error)
+//     }
+//     else {
+//         console.log('Server is listening on port ' + port)
+//     }
+// })
+
+const users = {}
 
 io.on('connection', socket => {
     // socket.emit('chat-message', 'Welcome')
@@ -9,5 +51,8 @@ io.on('connection', socket => {
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
     })
-    
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
 })
