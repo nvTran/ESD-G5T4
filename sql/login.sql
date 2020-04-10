c3796f9a72a7e6f54edcedf1f8a201995b754db7
sebastianhkx <kxhong.2018@sis.smu.edu.sg>
2020-04-10 17:41:35 +0800

    tele comment box

---
+++ sql/login.sql
@@ -0,0 +1,9 @@
+
+-- create table for storing user data 
+CREATE TABLE login (
+id int(9) NOT NULL auto_increment,
+name varchar(100) NOT NULL,
+email varchar(100) NOT NULL,
+password varchar(100) ,
+PRIMARY KEY (id)
+);
